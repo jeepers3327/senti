97ad36b9bc8b11d0e09d490126b6d6a7509620c7
Jhefrey Sajot <heifurukawa1997@gmail.com>
2021-04-28 22:41:09 +0800

    feat: add index page

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -1,67 +1,39 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import { Content } from '@/components';
+
+import {
+  useColorModeValue,
+  Button,
+  VStack,
+  Box,
+  Heading,
+  Input,
+  Flex,
+} from '@chakra-ui/react';
+import React from 'react';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{` `}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <p className={styles.description}>This is not an official starter!</p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=typescript-nextjs-starter"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=typescript-nextjs-starter"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Content title="Senti - Join presentation" hasNavbar>
+      <Flex p="5rem" flex="1" direction="column" alignItems="center">
+        <Box
+          p="10"
+          rounded="lg"
+          border="1px solid #d3dae6"
+          shadow="lg"
+          bg={useColorModeValue(`white`, `gray.800`)}
+          minW="xl"
         >
-          Powered by{` `}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+          <VStack spacing="5">
+            <Heading pt="5" size="md">
+              Enter the access code below to join!
+            </Heading>
+            <Input placeholder="Enter access code..." />
+            <Button colorScheme="blue" isFullWidth>
+              Join Session
+            </Button>
+          </VStack>
+        </Box>
+      </Flex>
+    </Content>
   );
 }
