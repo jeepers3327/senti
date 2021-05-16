aaa48a75094f4b5c246eafeebe7c6f8cf2e096a6
Jhefrey Sajot <heifurukawa1997@gmail.com>
2021-05-16 15:47:10 +0800

    feat: add top bars for Router state changes

--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -1,12 +1,50 @@
-import { AppProps } from 'next/app';
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
+import { useEffect, useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
+import { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
 
+import { SocketProvider } from '@/contexts';
+import { store } from '@/store';
 import '@/styles/global.css';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+const progress = new ProgressBar({
+  size: 2,
+  color: `#3182ce`,
+  className: `bar-of-progress`,
+  delay: 100,
+});
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const [isMounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+
+    Router.events.on(`routeChangeStart`, progress.start);
+    Router.events.on(`routeChangeComplete`, progress.finish);
+    Router.events.on(`routeChangeError`, progress.finish);
+    return () => {
+      Router.events.off(`routeChangeStart`, progress.start);
+      Router.events.off(`routeChangeComplete`, progress.finish);
+      Router.events.off(`routeChangeError`, progress.finish);
+    };
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
   return (
     <ChakraProvider>
-      <Component {...pageProps} />
+      <SocketProvider wsUrl="ws://localhost:4000/socket">
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
+      </SocketProvider>
     </ChakraProvider>
   );
-}
+};
+
+export default App;
--- app/src/pages/_document.tsx
+++ app/src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import theme from '@/util/theme';
+import { theme } from '@/utils';
 
 export default class Document extends NextDocument {
   render() {
